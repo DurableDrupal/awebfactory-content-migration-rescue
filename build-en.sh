258d143e171a4ea474ac086e5cc7f8bfa14a9c4f
Victor Kane <victorkane@gmail.com>
2017-06-25 15:18:41 -0300

    consistency in file naming

--- build-en.sh
+++ build-en.sh
@@ -1,19 +1,19 @@
 pandoc manuscript/en/F01.md \
-  manuscript/en/I01-Introduction.md \
+  manuscript/en/I01-introduction.md \
   manuscript/en/C01-title.md \
   manuscript/en/C02-title.md \
   manuscript/en/B01-bibliography.md \
   -S -o _preview/en/awebfactory-content-migration-rescue.html --toc --toc-depth=2
 
 pandoc manuscript/en/F01.md \
-  manuscript/en/I01-Introduction.md \
+  manuscript/en/I01-introduction.md \
   manuscript/en/C01-title.md \
   manuscript/en/C02-title.md \
   manuscript/en/B01-bibliography.md \
   -S -o _preview/en/awebfactory-content-migration-rescue.pdf --toc --toc-depth=2 --latex-engine=xelatex
 
 pandoc manuscript/en/F01.md \
-  manuscript/en/I01-Introduction.md \
+  manuscript/en/I01-introduction.md \
   manuscript/en/C01-title.md \
   manuscript/en/C02-title.md \
   manuscript/en/B01-bibliography.md \
