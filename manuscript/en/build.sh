96f67fe5053ddd47e4dc19fd380ff40db2bb88fc
Victor Kane <victorkane@gmail.com>
2017-10-18 00:25:27 -0300

    Create complete book with toc on a single page markdown file

--- manuscript/en/build.sh
+++ manuscript/en/build.sh
@@ -1,7 +1,10 @@
 toc=$(<manuscript/en/toc.txt)
 
 # html
-pandoc ${toc} -S -o _preview/en/awebfactory-content-migration-rescue.html --toc --toc-depth=4
+pandoc ${toc} -S -s -o _preview/en/awebfactory-content-migration-rescue.html --toc --toc-depth=4
+
+# md to md
+pandoc ${toc} -S -s -o _preview/en/awebfactory-content-migration-rescue.md --toc --toc-depth=4
 
 # pdf
 pandoc ${toc} -S -o _preview/en/awebfactory-content-migration-rescue.pdf --toc --toc-depth=4 --latex-engine=xelatex
